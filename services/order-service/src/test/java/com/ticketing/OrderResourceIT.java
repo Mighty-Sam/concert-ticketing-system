0565245a54237b9dc11e2f23c221bcf7d56eca33
sam.lee <Sam.Lee@teamp1us.com.tw>
2025-04-22 17:04:23 +0800

    feat: 1. 創建 payment, order, notification, event 服務模組

---
+++ services/order-service/src/test/java/com/ticketing/OrderResourceIT.java
@@ -0,0 +1,8 @@
+package com.ticketing;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class OrderResourceIT extends OrderResourceTest {
+    // Execute the same tests but in packaged mode.
+}
---
+++ services/payment-service/src/main/java/com/ticketing/PaymentResource.java
@@ -0,0 +1,16 @@
+package com.ticketing;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/payments")
+public class PaymentResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
